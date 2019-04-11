da3d8bf58583266427755034bbc0aa13aa59adee
Nazarii Piontko <1506550+nazarii-piontko@users.noreply.github.com>
2019-04-11 23:25:53 +0300

    Fix travis build failed due to chrome

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -6,6 +6,12 @@ export APP_BASE_URI=http://app.test/
 export SELENIUM_REMOTE=True
 export ARTIFACTS_DIR=/artifacts
 
-dockerd > /dev/null 2>&1 &
+if pgrep -x dockerd > /dev/null
+then
+    echo 'Docker daemon is running'
+else
+    echo 'Starting Docker daemon'
+    dockerd > /dev/null 2>&1 &
+fi
 
 python3 -m behave
