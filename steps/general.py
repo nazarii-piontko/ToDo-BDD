14bbd7dcf2d3a7187a44bbe55befaebd04493681
Nazarii Piontko <1506550+nazarii-piontko@users.noreply.github.com>
2019-04-26 19:14:00 +0300

    Fix regex statement in wait step

--- steps/general.py
+++ steps/general.py
@@ -6,13 +6,11 @@ from infrastructure.page import Page
 use_step_matcher('re')
 
 
-@when(u'(?:I )?wait for {seconds:d}')
-@then(u'(?:I )?wait for {seconds:d}')
+@when(u'(?:I )?wait for (?P<seconds>\\d+) seconds?')
 def wait(context, seconds):
     reg(context)[Page].wait(float(seconds))
 
 
 @when(u'(?:I )?create screenshot')
-@then(u'(?:I )?create screenshot')
 def create_screenshot(context):
     reg(context)[Page].create_screenshot()
